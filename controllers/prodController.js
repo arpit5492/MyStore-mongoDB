57eea9f48915303ed14ba07b00bd707319eb107d
arpit5492 <arpitmallick99@gmail.com>
2024-03-27 21:41:03 -0700

    prod-controller updated

--- controllers/prodController.js
+++ controllers/prodController.js
@@ -4,7 +4,7 @@ import { Product } from "../db/product.js";
 const getAllProds = async (req, res) => {
     try{
         const products = await Product.find();
-        console.log((products));
+        // console.log((products));
         res.render("home", {title: "Home", products: products, isLoggedIn: global.isLoggedIn});
     } catch(err) {
         console.log(err);
@@ -14,8 +14,8 @@ const getAllProds = async (req, res) => {
 //Showing the edit product page
 const editProds = async (req, res) => {
     try{
-        const product = await Product.findByPk(req.params.id);
-        console.log(JSON.stringify(product, null, 2));
+        const product = await Product.findById(req.params.id);
+        console.log(product);
         if(product){
             res.render("editProd", {title: "Edit Product", prod: product, isLoggedIn: global.isLoggedIn});
         } else {
@@ -35,15 +35,19 @@ const updateEachProd = async (req, res) => {
     const id = req.params.id;
     // console.log(id, prod_name, price, image);
     try{
-        await Product.update({
-            pName: prod_name,
-            price: price,
-            image: img
-        },
-        {where: {id: id}}
-        );
-        console.log("Product updated");
-        res.redirect("/");
+        const prod = await Product.findById(id);
+        if(prod){
+            prod.pName = prod_name;
+            prod.price = price;
+            prod.image = img;
+            
+            await prod.save();
+            console.log("Product editted successfully!!");
+            res.redirect("/");
+        } else {
+            console.log("Product not found");
+            res.status(400).send("Product not found");
+        }
     } catch (err) {
         console.log(err);
     }
@@ -59,12 +63,12 @@ const postAddProd = async (req, res) => {
     const {prod_name, price} = req.body;
     const img = req.file.filename;
     try{
-        await Product.create({
+        const addProd = await Product.create({
             pName: prod_name,
             price: price,
             image: img
         });
-        console.log("Product added successfully!!");
+        console.log("Product added successfully!!", addProd);
         res.redirect("/");
     } catch(err) {
         console.log(err);
@@ -75,9 +79,7 @@ const postAddProd = async (req, res) => {
 const delProd = async (req, res) => {
     const id = req.params.id;
     try{
-        await Product.destroy(
-            {where: {id: id}}
-        )
+        await Product.findByIdAndDelete(id);
         console.log("Product deleted successfully!!");
         res.redirect("/");
     } catch(err) {
